f43778f2a4e0770962bda0e01096d9178a1e4c72
Yann Jorelle <yannjorelle@protonmail.com>
2021-04-05 12:15:47 +0300

    Improve chroot script
    Adds custom systemd-boot loader configurations. Also adds a basic /etc/hosts
    configuration.

--- chroot.sh
+++ chroot.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # Script that is intended to be run after chrooting into a new system. Installs
-# systemd-boot as the boot manager and copies example loader configurations.
+# systemd-boot as the boot manager.
 # Most commands are from: https://wiki.archlinux.org/index.php/Installation_guide
 
 ### Global variables
 boot_path="/boot"
+root_partition="/dev/sda2"
 time_zone="Europe/Helsinki"
 locale="en_US.UTF-8 UTF-8"
 lang="LANG=en_US.UTF-8"
@@ -14,9 +15,24 @@ lang="LANG=en_US.UTF-8"
 # Install systemd-boot
 [ -d "$boot_path" ] || echo "Boot directory not found, exiting"; exit 1
 bootctl install
-# Copy example configurations
-cp /usr/share/systemd/bootctl/loader.conf /boot/loader/
-cp /usr/share/systemd/bootctl/arch.conf /boot/loader/entries/
+
+# Loader configuration
+# https://wiki.archlinux.org/index.php/Systemd-boot#Loader_configuration
+cat > /boot/loader/loader.conf <<EOF
+default arch.conf
+timeout 5
+console-mode max
+editor no
+EOF
+
+# Loader entry
+root_partition_uuid=$(lsblk -dno PARTUUID "$root_partition")
+cat > /boot/loader/entries/arch.conf <<EOF
+title   Arch Linux
+linux   /vmlinuz-linux
+initrd  /initramfs-linux.img
+options root=PARTUUID=$root_partition_uuid rw
+EOF
 
 # Set root password
 echo "Set a new root password"
@@ -38,3 +54,15 @@ systemctl enable NetworkManager
 # Set hostname
 read -p "Enter a hostname: " host
 echo "$host" > /etc/hostname
+
+# Set /etc/hosts configuration
+cat > /etc/hosts <<EOF
+#
+# hosts - static table lookup for hostnames
+#
+127.0.0.1  localhost
+127.0.1.1  $host
+::1        localhost ip6-localhost ip6-loopback
+ff02::1    ip6-allnodes
+ff02::2    ip6-allrouters
+EOF
