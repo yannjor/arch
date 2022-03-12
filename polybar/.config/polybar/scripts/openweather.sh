dfb6afe7e0cd143d4101648089ed660cc6d67ab7
Yann Jorelle <yannjorelle@protonmail.com>
2022-03-12 11:58:46 +0100

    Change i3 bindings and small polybar changes

--- polybar/.config/polybar/scripts/openweather.sh
+++ polybar/.config/polybar/scripts/openweather.sh
@@ -3,28 +3,28 @@
 get_icon() {
     case $1 in
         # Icons
-        01d) icon="" ;; # Clear sky day.
-        01n) icon="" ;; # Clear sky night.
-        02d) icon="" ;; # Few clouds day.
-        02n) icon="" ;; # Few clouds night.
-        03d) icon="" ;; # Scattered clouds day.
-        03n) icon="" ;; # Scattered clouds night.
-        04d) icon="" ;; # Broken clouds day.
-        04n) icon="" ;; # Broken clouds night.
-        09d) icon="" ;; # Shower rain day.
-        09n) icon="" ;; # Shower rain night.
-        10d) icon="" ;; # Rain day.
-        10n) icon="" ;; # Rain night
-        11d) icon="" ;; # Thunderstorm day.
-        11n) icon="" ;; # Thunderstorm night
-        13d) icon="" ;; # Snow day. Snowflake alternative: 
-        13n) icon="" ;; # Snow night. Snowflake alternative: 
-        50d) icon="" ;; # Mist day.
-        50n) icon="" ;; # Mist night.ather-icons
-        *) icon="" ;;
+        01d) icon="" ;; # Clear sky day.
+        01n) icon="" ;; # Clear sky night.
+        02d) icon="" ;; # Few clouds day.
+        02n) icon="" ;; # Few clouds night.
+        03d) icon="" ;; # Scattered clouds day.
+        03n) icon="" ;; # Scattered clouds night.
+        04d) icon="" ;; # Broken clouds day.
+        04n) icon="" ;; # Broken clouds night.
+        09d) icon="" ;; # Shower rain day.
+        09n) icon="" ;; # Shower rain night.
+        10d) icon="" ;; # Rain day.
+        10n) icon="" ;; # Rain night
+        11d) icon="" ;; # Thunderstorm day.
+        11n) icon="" ;; # Thunderstorm night
+        13d) icon="ﰕ" ;; # Snow day. Snowflake alternative: 
+        13n) icon="ﰕ" ;; # Snow night. Snowflake alternative: 
+        50d) icon="" ;; # Mist day.
+        50n) icon="" ;; # Mist night.ather-icons
+        *) icon="" ;;
     esac
 
-    echo $icon
+    echo "$icon"
 }
 
 KEY=""
@@ -56,6 +56,5 @@ fi
 if [ -n "$weather" ]; then
     weather_temp=$(echo "$weather" | jq ".main.temp" | cut -d "." -f 1)
     weather_icon=$(echo "$weather" | jq -r ".weather[0].icon")
-
-    echo "$(get_icon "$weather_icon")" "$weather_temp$SYMBOL"
+    printf "%s  %s%s" "$(get_icon "$weather_icon")" "$weather_temp" "$SYMBOL"
 fi
