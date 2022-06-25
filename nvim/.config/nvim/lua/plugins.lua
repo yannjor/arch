304c034748357ab696e095c8b0bd92ffff0a0080
Yann Jorelle <yannjorelle@proton.me>
2022-06-25 21:00:01 +0200

    nvim: autoinstall packer

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1,3 +1,12 @@
+-- Install packer automatically
+local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+local is_bootstrap = false
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+    is_bootstrap = true
+    vim.fn.execute("!git clone https://github.com/wbthomason/packer.nvim " .. install_path)
+    vim.cmd([[packadd packer.nvim]])
+end
+
 require("packer").startup(function(use)
     -- Packer can manage itself
     use("wbthomason/packer.nvim")
@@ -78,7 +87,20 @@ require("packer").startup(function(use)
     use("marko-cerovac/material.nvim")
     -- Catppuccin
     use({ "catppuccin/nvim", as = "catppuccin" })
+
+    if is_bootstrap then
+        require("packer").sync()
+    end
 end)
+
+if is_bootstrap then
+    print("==================================")
+    print("    Plugins are being installed")
+    print("    Wait until Packer completes,")
+    print("       then restart nvim")
+    print("==================================")
+    return
+end
 
 -- Configuration and setup of plugins
 require("impatient")
